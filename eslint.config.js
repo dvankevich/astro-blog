f1d72e32b03792dca5ec13eff7ba8b31d45791de
dvankevich <dvankevich@gmail.com>
2026-02-08 21:30:24 +0200

    feat: Create a blog post archive

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,28 @@
 import eslintPluginAstro from "eslint-plugin-astro";
+import tseslint from "typescript-eslint";
+import tsParser from "@typescript-eslint/parser";
+
 export default [
-  // add more generic rule sets here, such as:
-  // js.configs.recommended,
+  // Додаємо рекомендовані налаштування для TS
+  ...tseslint.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
+
+  {
+    files: ["**/*.astro"],
+    languageOptions: {
+      parser: eslintPluginAstro.parser,
+      parserOptions: {
+        // Оце критично важливий момент:
+        // Ми кажемо Astro-парсеру використовувати TS-парсер для скриптів всередині
+        parser: tsParser,
+        extraFileExtensions: [".astro"],
+        sourceType: "module",
+      },
+    },
+  },
   {
     rules: {
-      // override/add rules settings here, such as:
-      // "astro/no-set-html-directive": "error"
+      // Ваші кастомні правила
     },
   },
 ];
