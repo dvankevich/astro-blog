885ad112477a3871f9c4bf3343c3a9546ad1544a
dvankevich <dvankevich@gmail.com>
2026-02-11 18:43:21 +0200

    feat: Build first Astro island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 export default defineConfig({
   site: "https://tutorial-first-astro-blog.netlify.app",
+  integrations: [preact()],
 });
